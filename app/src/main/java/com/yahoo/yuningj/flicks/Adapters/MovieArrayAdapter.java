327f3ac0991a0fb8e91548853a00a67732c11ba9
Yuning Jin <yuningj@yahoo-inc.com>
2016-11-07 17:26:40 -0800

    title and overview loaded

---
+++ app/src/main/java/com/yahoo/yuningj/flicks/Adapters/MovieArrayAdapter.java
@@ -0,0 +1,8 @@
+package com.yahoo.yuningj.flicks.Adapters;
+
+/**
+ * Created by yuningj on 11/7/16.
+ */
+
+public class MovieArrayAdapter {
+}
---
+++ app/src/main/java/com/yahoo/yuningj/flicks/Models/Movie.java
@@ -0,0 +1,8 @@
+package com.yahoo.yuningj.flicks.Models;
+
+/**
+ * Created by yuningj on 11/7/16.
+ */
+
+public class Movie {
+}
